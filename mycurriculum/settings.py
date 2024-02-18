5904d8dc60bf17c002e506af0faf97151a36644a
Lawal Yusuf <lawalusersive@gmail.com>
2024-02-18 16:54:48 +0100

    adjustment down

--- mycurriculum/settings.py
+++ mycurriculum/settings.py
@@ -155,9 +155,9 @@ STATIC_URL = '/static/'
 
 STATIC_URL = "static/"
 STATIC_ROOT=BASE_DIR /'static'
-STATICFILES_DIRS=[
-    'mycurriculum/static',
-]
+# STATICFILES_DIRS=[
+#     'mycurriculum/static',
+# ]
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
