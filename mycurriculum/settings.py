c6d552388bf823e50eff1cad0fa55788dc6e83ec
Lawal Yusuf <lawalusersive@gmail.com>
2024-02-18 16:56:11 +0100

    t down

--- mycurriculum/settings.py
+++ mycurriculum/settings.py
@@ -161,7 +161,7 @@ STATIC_ROOT=BASE_DIR /'static'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
