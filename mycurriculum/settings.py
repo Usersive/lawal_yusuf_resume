5008982927b0b6ea85095597d3ed5bc0c2c4f352
Lawal Yusuf <lawalusersive@gmail.com>
2024-02-18 05:03:47 +0100

    update debug

--- mycurriculum/settings.py
+++ mycurriculum/settings.py
@@ -87,12 +87,12 @@ if not DEBUG:
     }
 else:
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 
