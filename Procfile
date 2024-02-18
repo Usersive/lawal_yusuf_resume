f34bcf8d038fa7bd91435b5fa76267b98625fd07
Lawal Yusuf <lawalusersive@gmail.com>
2024-02-18 03:48:27 +0100

    update

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn mycurriculum.wsgi --log-file -
--- mycurriculum/settings.py
+++ mycurriculum/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMidlleWare',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -159,6 +160,7 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressdManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
