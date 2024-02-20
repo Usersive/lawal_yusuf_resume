7d4b0712f5d273a31ba4902b4c31598a29d47fce
Lawal Yusuf <lawalusersive@gmail.com>
2024-02-20 20:13:50 +0100

    total

--- mycurriculum/urls.py
+++ mycurriculum/urls.py
@@ -21,8 +21,8 @@ from django.urls import path, include
 from .import views
 
 urlpatterns = [
-    path('dadmin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
-    path('admin/', admin.site.urls),
+    path('fakeadmin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('horenadmin/', admin.site.urls),
     path('', views.index, name='index'),
     path('cvitae/', include('cvitae.urls')),
      path('current_year', views.current_year, name='current_year'),
