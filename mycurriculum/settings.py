bf41bc3069374851b86e5d798bcfea040ba69699
Lawal Yusuf <lawalusersive@gmail.com>
2024-02-21 13:17:23 +0100

    image

--- mycurriculum/settings.py
+++ mycurriculum/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 
 
 
-STATIC_URL = "static/"
-STATIC_ROOT=BASE_DIR /'static'
+STATIC_URL = "/static/"
+# STATIC_ROOT=BASE_DIR /'static'
 STATICFILES_DIRS=[
     'mycurriculum/static',
 ]
@@ -148,12 +148,12 @@ STATICFILES_DIRS=[
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = '/media/'
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS ={
