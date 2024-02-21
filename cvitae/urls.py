1641f2824f02abc2846335b1c20b1b3de3d794ba
Lawal Yusuf <lawalusersive@gmail.com>
2024-02-21 14:22:27 +0100

    changes

--- cvitae/urls.py
+++ cvitae/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns =[
     path('email_compose/', views.email_compose, name='email_compose'),
@@ -9,3 +9,5 @@ urlpatterns =[
     path('portfolio/', views.portfolio, name='portfolio'),
     path('project/', views.project, name='project'),
 ]
+urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
