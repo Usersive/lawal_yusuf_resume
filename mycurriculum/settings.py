db76cbd042e718d1d12de1a4b15aab4acd5d92a4
Lawal Yusuf <lawalusersive@gmail.com>
2024-02-22 17:55:11 +0100

    external

--- mycurriculum/settings.py
+++ mycurriculum/settings.py
@@ -89,16 +89,16 @@ AUTH_USER_MODEL = 'cvitae.Account'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("DB_NAME"),
+#         'USER': env("DB_USER"),
+#         'PASSWORD': env("DB_PASSWORD"),
+#         'HOST': env("DB_HOST"),
+#         'PORT': env("DB_PORT"),
+#     }
+# }
 
 if not DEBUG:
     DATABASES={
