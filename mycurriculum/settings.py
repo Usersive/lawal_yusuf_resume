0b3947107e3d85ec8c0c9fbb370754cf32b3e1d9
Lawal Yusuf <lawalusersive@gmail.com>
2024-02-29 19:57:11 +0100

    add me

--- mycurriculum/settings.py
+++ mycurriculum/settings.py
@@ -159,7 +159,7 @@ STATICFILES_DIRS=[
 ]
 STATIC_ROOT=BASE_DIR /'static'
 STATIC_ROOT=BASE_DIR /'staticfiles'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
