f9deb0981eda667811e5770041e308b1b5316d8f
Lawal Yusuf <lawalusersive@gmail.com>
2024-02-29 11:34:13 +0100

    work for

--- cvitae/models.py
+++ cvitae/models.py
@@ -159,7 +159,7 @@ class Project(models.Model):
 class Portfolio(models.Model):
     title   = models.CharField(max_length=30)
     description = models.CharField(max_length=200)
-    image   = models.ImageField(null=True, blank=True, upload_to='portfolioimages/')
+    image   = models.ImageField(upload_to='portfolioimages/')
     
     def __str__(self):
         return self.title
